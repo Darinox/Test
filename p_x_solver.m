9e0d13f01f317def89a935f9a1256653344eea8a
Darinox <timothee.felicio@gmail.com>
2017-09-30 17:12:01 +0200

    30.09.erste_plots

--- p_x_solver.m
+++ p_x_solver.m
@@ -1,4 +1,4 @@
-function [ Nullsein, ] = p_x_solver( R, x, n, x_gewaehlt, p)
+function [ Nullsein ] = p_x_solver( R, x, n, x_gewaehlt, p)
 % p muss mit rein
 tic
 n1 = 4;
@@ -11,23 +11,23 @@ Teil_1_oben = 0;
 for k1 = [1:1:K]
    Teil_1_oben =  Teil_1_oben + q(k1)*vk_1(x, k1, x_i, H_n1, n1, p);
 end
-fprintf('Teil_1_oben =  ');
-fprintf('%d \n', Teil_1_oben);
+%fprintf('Teil_1_oben =  ');
+%fprintf('%d \n', Teil_1_oben);
 
 Teil_1_unten = p * log(2);
-fprintf('Teil_1_unten =  ');
-fprintf('%d \n', Teil_1_unten);
+%fprintf('Teil_1_unten =  ');
+%fprintf('%d \n', Teil_1_unten);
 
 Sum_vk_2 = 0;
 for k1 = [1:1:K]
     Sum_vk_2 = Sum_vk_2 + q(k1)*vk_2(x, k1, x_i, H_n1, n1, p);
 end
-fprintf('Sum _vk_2 =  ');
-fprintf('%d \n', Sum_vk_2);
+%fprintf('Sum_vk_2 =  ');
+%fprintf('%d \n', Sum_vk_2);
 
 oo_1 = o_1(x, n);
-fprintf('o_1 =  ');
-fprintf('%d \n', oo_1);
+%fprintf('o_1 =  ');
+%fprintf('%d \n', oo_1);
 
 Teil_2 = x(x_gewaehlt)*sqrt(2/n*Sum_vk_2) /(p*log(2));
 %out = Teil_1_oben;
--- vk_2.m
+++ vk_2.m
@@ -2,8 +2,6 @@ function [ vk_2 ] = vk_2( x, k, x_i, polynom, n, p )
 %tic
 
 w = x(k);
-syms y;
-syms z;
 o = 1;
 q = [1/8,1/8,1/8,1/8,1/8,1/8,1/8,1/8];
 
@@ -34,7 +32,7 @@ H_n1 = 2^(n/2)*H_n1;
 for k1 = [1:1:n]
     w_i = (2^(k1-1)*factorial(k1)*sqrt(pi))/(k1^2*H_n1^2);
     
-    f_i = subs(   (1/sqrt(2*pi*o^2)^(1/(1+p)) * ((exp(-((y^2 -2*y*x +x.^2)/(2*o^2)))).^(1/(1+p))*q'))^p  ,    y, x_k1(k1)   );
+    f_i = (1/sqrt(2*pi*o^2)^(1/(1+p)) * ((exp(-((x_k1(k1)^2 -2*x_k1(k1)*x +x.^2)/(2*o^2)))).^(1/(1+p))*q'))^p;
     
     vk_1_unten =  vk_1_unten +A*sqrt(B)*w_i*f_i;
 end
@@ -42,8 +40,8 @@ end
 for k1 = [1:1:n]
     w_i = (2^(k1-1)*factorial(k1)*sqrt(pi))/(k1^2*H_n1^2);
     
-    f_i = subs(   (1/sqrt(2*pi*o^2)^(1/(1+p)) * ((exp(-((y^2 -2*y*x +x.^2)/(2*o^2)))).^(1/(1+p))*q'))^p  ,    y, x_k1(k1)   );
-    p_yk = subs(1/sqrt(2*pi*o^2) * exp(-((y.^2 -2*y'*w +w.^2)/(2*o^2))), y, x_k1(k1));
+    f_i = (1/sqrt(2*pi*o^2)^(1/(1+p)) * ((exp(-((x_k1(k1)^2 -2*x_k1(k1)*x +x.^2)/(2*o^2)))).^(1/(1+p))*q'))^p;
+    p_yk = 1/sqrt(2*pi*o^2) * exp(-((x_k1(k1).^2 -2*x_k1(k1)'*w +w.^2)/(2*o^2)));
     f_log = log(p_yk^(1/(1+p)) *f_i/p_yk);
     f_log2 = log(p_yk^(1/(1+p)) *f_i/p_yk)^2;
     vk_1_oben =  vk_1_oben + A*sqrt(B)*w_i*f_i*f_log;
